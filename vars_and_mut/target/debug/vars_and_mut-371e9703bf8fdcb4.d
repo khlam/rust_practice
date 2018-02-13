b0b37c4597f602546e5297fc159483e0e9fa9248
khlam <4841220+khlam@users.noreply.github.com>
2018-02-12 17:08:17 -0800

    clean

--- vars_and_mut/target/debug/vars_and_mut-371e9703bf8fdcb4.d
+++
@@ -1,1 +0,0 @@
-/home/lamki/Documents/rust_practice/vars_and_mut/target/debug/vars_and_mut-371e9703bf8fdcb4: /home/lamki/Documents/rust_practice/vars_and_mut/src/main.rs
--- vars_and_mut/target/debug/vars_and_mut.d
+++
@@ -1,1 +0,0 @@
-/home/lamki/Documents/rust_practice/vars_and_mut/target/debug/vars_and_mut: /home/lamki/Documents/rust_practice/vars_and_mut/src/main.rs
