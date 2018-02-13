1167071ecf09105fdb4c0a9a7365c379c41afcea
khlam <4841220+khlam@users.noreply.github.com>
2018-02-12 16:16:33 -0800

    Practice rust, guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,35 @@
+extern crate rand;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 100);
+
+    println!("Guess a number!");
+   
+   	loop { 
+       println!("Guess: ");
+   
+       let mut guess = String::new();
+   
+       io::stdin().read_line(&mut guess)
+       	.expect("Failed to read line");
+   
+       let guess: u32 = match guess.trim().parse() {
+       		Ok(num) => num,
+       		Err(_)	=> continue,
+       };
+   
+      	println!("You guessed {}", guess);
+   
+      	match guess.cmp(&secret_number) {
+      		Ordering::Less 		=> println!("Too small!"),
+      		Ordering::Greater 	=> println!("Too Big!"),
+      		Ordering::Equal 	=> {
+      			println!("WINNER WINNER CHICKEN DINNER! Number: {}", secret_number);
+      			break;
+      		}
+      	}     	
+    }
+}
