e1c570b85a394e0e9ad291c11f3dc597f9269f9f
khlam <4841220+khlam@users.noreply.github.com>
2018-06-04 13:12:12 -0700

    simple webserver

---
+++ webserver/target/debug/deps/webserver-426100f1fd848569.d
@@ -0,0 +1,5 @@
+C:\Users\khlam\Documents\rust_practice\webserver\target\debug\deps\webserver-426100f1fd848569.exe: src\main.rs
+
+C:\Users\khlam\Documents\rust_practice\webserver\target\debug\deps\webserver-426100f1fd848569.d: src\main.rs
+
+src\main.rs:
---
+++ webserver/target/debug/webserver.d
@@ -0,0 +1,1 @@
+C:\Users\khlam\Documents\rust_practice\webserver\target\debug\webserver.exe: C:\Users\khlam\Documents\rust_practice\webserver\src\main.rs
