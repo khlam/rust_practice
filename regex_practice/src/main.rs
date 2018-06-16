c4238b919b3fb91c50206a212fed49b5c3399e4b
khlam <4841220+khlam@users.noreply.github.com>
2018-06-16 15:11:35 -0700

    new approach

--- regex_practice/src/main.rs
+++ regex_practice/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
         line_num = line_num + 1;
         let question_string = line.unwrap();
         if check_line(&question_string, &line_num) {
-            //println!("\n{:?}", question_string);
+            get_study_question(&question_string, &line_num);
         }
     }
 }
@@ -22,7 +22,7 @@ fn main() {
 fn check_line(line: &String, line_num: &u8) -> bool {
     let line_arr: Vec<char> = line.chars().collect();
     if line_arr.len() == 0 {      // Check for newline
-        println!("LINE\t{:?}\tWARNING: Newline/Empty line", line_num);
+        // println!("LINE\t{:?}\tWARNING: Newline/Empty line", line_num);
         return false;
     }
 
@@ -33,22 +33,35 @@ fn check_line(line: &String, line_num: &u8) -> bool {
 
     if line.chars().filter(|&c| c == ';').count() < 3 {
         panic!("\n\tERROR: line {:?}\t Missing \';\' delimiter.", line_num);
-    }
-
-    // Match Summary Regex
-    let summary_re = Regex::new("^([0-9]+|summary);[0-9]+;plain:NA:NA;").unwrap();
-    if summary_re.is_match(line) {
-        println!("LINE\t{:?}\tSUMMARY REGEX MATCH", line_num);
-    } else{
-        // Wait For Click Regex
-        let wait_for_click_re = Regex::new("^[0-9]+;[0-9]+;waitForClick:(gameboard_|rewardBar_|saliencyMap_)*(gameboard_|rewardBar_|saliencyMap_)*(gameboard:|rewardBar:|saliencyMap:)+.+;").unwrap();
-        if (wait_for_click_re.is_match(line)){
-            println!("LINE\t{:?}\tWAIT FOR CLICK REGEX MATCH", line_num);
-        } else{
-            println!("LINE\t{:?}\tREGEX MISMATCH", line_num);
-        }
-    }        
-
-    
+    }       
     true
+}
+
+fn get_study_question(line: &String, line_num: &u8) {
+    let line_parts_iterator = line.split(";");
+    let vec: Vec<&str> = line_parts_iterator.collect();
+
+    let step: String = vec[0].to_string();
+    check_regex("^[0-9]+$|^summary$".to_string(), "STEP REGEX FAIL".to_string(), &step, &line_num);
+
+    let question_index: String = vec[1].to_string();
+    check_regex("^[0-9]+$".to_string(), "INDEX REGEX FAIL".to_string(), &question_index, &line_num);
+    
+    let question_type: String = vec[2].to_string();
+    check_regex("plain:NA:NA|^waitForClick:(gameboard_|rewardBar_|saliencyMap_){0, 2}(gameboard|rewardBar|saliencyMap){0, 1}:.+".to_string(), "QUESTION TYPE REGEX FAIL".to_string(), &question_type, &line_num);
+
+    let question : String = vec[3].to_string();
+    
+    let mut answer_vec : Vec<String> = Vec::new();
+    for x in 4..vec.len() {
+        answer_vec.push(vec[x].to_string());
+    }
+}
+
+fn check_regex(regex: String, msg: String, line: &String, line_num: &u8) {
+    let step_reg = Regex::new(&regex).unwrap();
+    if !step_reg.is_match(&line) {                      // on Regex no match, panic
+        println!("LINE\t{:?}\t{:?}", line_num, msg);
+       // return true;
+    }
 }
